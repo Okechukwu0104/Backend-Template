15d938e892da60e3ee178ffddf21c046e8640361
Okechukwu0104 <okechukwupeter10@gmail.com>
2023-04-22 14:44:02 +0100

    all changes

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const {handler} = require("./errors");
+
+//let us call some inbuilt
+app.use(
+  express.urlencoded({
+    limit: "100mb",
+    extended: true,
+  })
+);
+
+app.use((req, res, next) => {
+  console.log("request received", req.method, req.ur1);
+  next();
+});
+
+//error handlers
+app.use(handler);
+
+module.exports = app;
